e26de4e80385be6767a1ad296ac038ed5a4ea399
Ritesh <riteshpersaud2003@gmail.com>
2024-02-11 22:27:25 -0500

    Test was Successful

--- src/pages/WhyHomes.js
+++ src/pages/WhyHomes.js
@@ -47,7 +47,7 @@ export default function WhyHomes() {
         </div>
         <div className="section">
           <p className="hero gradient-text" style={{ textAlign: "center" }}>
-            Beat Inflation, Defy Recession
+            Beat Inflation, Defy Recessions
           </p>
           <div className="grid">
             <div className="grid-element">
